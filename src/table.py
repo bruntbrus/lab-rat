4999ffd5130d0b882d87d04e63b5891346d4bbbc
bruntbrus <tomas.enarsson@outlook.com>
2021-01-27 17:35:29 +0100

    Make Table class

---
+++ src/table.py
@@ -0,0 +1,14 @@
+'''
+Table class.
+'''
+
+class Table:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
